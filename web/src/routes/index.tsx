d7b09bf3a962ed27242d0337c54b52c6318dec69
alvarojunior02 <alvaroamcjunior8@gmail.com>
2022-08-01 16:35:58 -0400

    Resolve erro da tela de alterar produto

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -10,7 +10,7 @@ const RoutesHub: React.FC = () => {
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/createProduct" element={<CreateProduct />} />
-        <Route path="/updateProduct" element={<UpdateProduct />} />
+        <Route path="/updateProduct/:id" element={<UpdateProduct />} />
     </Routes>
   );
 };
